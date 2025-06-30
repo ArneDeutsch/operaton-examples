5650eb671c2700247a3f30e7e7a81559c7e1ac3b
Arne Deutsch <Arne.Deutsch@itemis.de>
2025-06-30 12:43:32 +0200

    Annotate Spring Boot applications

--- 1.0.0-beta-4/spring-boot/oracle/src/main/java/com/example/operaton/Application.java
+++ 1.0.0-beta-4/spring-boot/oracle/src/main/java/com/example/operaton/Application.java
@@ -1,6 +1,9 @@
 package com.example.operaton;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /** Marker application class used by tests to load resources. */
+@SpringBootApplication
 public class Application {
   // entry point can be added if required
 }
