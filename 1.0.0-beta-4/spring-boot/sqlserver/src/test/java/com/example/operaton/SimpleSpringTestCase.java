78894749c989abb3aa1299dcb05c46b57b6b1835
Arne Deutsch <Arne.Deutsch@itemis.de>
2025-06-30 15:58:58 +0200

    Load per-module config in spring tests

--- 1.0.0-beta-4/spring-boot/sqlserver/src/test/java/com/example/operaton/SimpleSpringTestCase.java
+++ 1.0.0-beta-4/spring-boot/sqlserver/src/test/java/com/example/operaton/SimpleSpringTestCase.java
@@ -3,13 +3,13 @@ package com.example.operaton;
 import org.operaton.bpm.engine.spring.test.Spring5TestHelper;
 import org.operaton.bpm.engine.spring.test.SpringProcessEngineTestCase;
 import org.operaton.bpm.engine.spring.test.SpringTestHelper;
-import org.operaton.bpm.engine.spring.test.ExampleConfiguration;
+
 import org.springframework.test.context.ContextConfiguration;
 
 /**
  * Minimal adapter returning the default test helper to avoid ServiceLoader lookup.
  */
-@ContextConfiguration(classes = ExampleConfiguration.class)
+@ContextConfiguration(locations = "classpath:operaton.cfg.xml")
 abstract class SimpleSpringTestCase extends SpringProcessEngineTestCase {
   @Override
   protected SpringTestHelper lookupTestHelper() {
